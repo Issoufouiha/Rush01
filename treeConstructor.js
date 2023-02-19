8913d15574a26993c7f25ff6996d8ab17114a859
Issoufouiha <124552114+Issoufouiha@users.noreply.github.com>
2023-02-19 18:49:08 +0100

    Create treeConstructor.js

---
+++ treeConstructor.js
@@ -0,0 +1,33 @@
+let strArr1 = ["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"];
+let strArr2 = ["(1,2)", "(3,2)", "(2,12)", "(5,2)"];
+function treeConstructor (strArr){
+
+    let parents={};
+    let children ={};
+
+    for (i=0; i<strArr.length; i++){
+        let pair = strArr[i].replace(/[()]/g,"").split(",");
+        let child = pair[0];
+        let parent = pair[1];
+
+        if (parents[parent]){
+            parents[parent].push(child);
+        }else{
+            parents[parent] = [child];
+        }
+
+        if (parents[parent].length > 2){
+            return false
+        }
+        if (children[child]){
+            return false;
+        }else{
+            children[child] = parent;
+        }
+    }
+    return true;
+}
+console.log("Entrée: ", strArr1);
+console.log("sortie: ", treeConstructor(strArr1));
+console.log("Entrée: ", strArr2);
+console.log("sortie: ", treeConstructor(strArr2));
