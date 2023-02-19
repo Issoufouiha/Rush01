eaab6235fc25586fedfbbbc413aae3aabeaa813b
Issoufouiha <124552114+Issoufouiha@users.noreply.github.com>
2023-02-19 18:44:21 +0100

    Create printBox.js

---
+++ printBox.js
@@ -0,0 +1,20 @@
+function printBox (width,height){
+  let lign="";
+      for (let i=1;i<=height;i++){
+              for (let j=1;j<=width;j++){
+                   if((i==1 && j<=width) || (i==height && j<=width)) {
+                      lign=lign+"*";
+                   }
+                   if ((i>1 && i < height && j==1) || (i>1 && i < height &&j == width)){
+                    lign=lign+"*";
+                   }
+                   if(i>1 && i < height && j>1 && j<width) {
+                      lign=lign+" ";
+                  }
+              }
+              console.log(lign);
+               lign="";
+       }           
+}
+printBox(6,5);
+module.exports = printBox
